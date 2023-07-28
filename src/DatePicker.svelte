c813a4b1220f22e706e127819c16386b27f00031
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-07-28 19:33:39 +0530

    ufff but not working though

--- src/DatePicker.svelte
+++ src/DatePicker.svelte
@@ -1,111 +1,58 @@
-<!-- src/CalendarPicker.svelte -->
-<script>
-  import { onMount, writable } from "svelte/store";
-  import { getWeek } from "./util/date";
+<script lang="ts">
+  import { onMount } from 'svelte';
 
-  export let selectedDate = null;
+  let selectedDate = new Date().toISOString().substring(0, 10);
+  let displayedDate = new Date();
 
-  let currentDate = new Date();
-  let currentMonth = currentDate.getMonth();
-  let currentYear = currentDate.getFullYear();
-
-  // Function to update the selected date
-  function updateSelectedDate(date) {
-    selectedDate = date;
+  function prevMonth() {
+    displayedDate.setMonth(displayedDate.getMonth() - 1);
+    updateDays();
+  }
+  
+  function nextMonth() {
+    displayedDate.setMonth(displayedDate.getMonth() + 1);
+    updateDays();
+  }
+  
+  function selectDate(day: number) {
+    let date = new Date(displayedDate.getFullYear(), displayedDate.getMonth(), day);
+    selectedDate = date.toISOString().substring(0, 10);
   }
 
-  // Function to change the month and year
-  function changeMonthYear(month, year) {
-    currentMonth = month;
-    currentYear = year;
-    weeks.set(getWeek(currentMonth, currentYear));
-  }
-
-  let weeks = writable([]); // Store to hold the weeks in the calendar
-
+  let days = [];
+  
   onMount(() => {
-    weeks.set(getWeek(currentMonth, currentYear));
+    updateDays();
+  });
+
+  $: displayedDate.getMonth(), displayedDate.getFullYear(), updateDays();
+
+  function updateDays() {
+    let startDay = new Date(displayedDate.getFullYear(), displayedDate.getMonth(), 1).getDay();
+    let daysInMonth = 32 - new Date(displayedDate.getFullYear(), displayedDate.getMonth(), 32).getDate();
+
+    // Adjust startDay according to the given week structure (starting on Tuesday)
+    startDay = (startDay + 6) % 7;
+  
+    days = [];
+    for(let i = 0; i < startDay; i++) {
+      days.push(null);
+    }
+    for(let i = 1; i <= daysInMonth; i++) {
+      days.push(i);
+    }
   }
 </script>
 
-<!-- src/CalendarPicker.svelte -->
-<style>
-  .calendar {
-    font-family: Arial, sans-serif;
-    max-width: 320px;
-    margin: 0 auto;
-  }
-
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 8px 16px;
-    background-color: #f0f0f0;
-  }
-
-  .header button {
-    cursor: pointer;
-    background: none;
-    border: none;
-    font-size: 16px;
-    font-weight: bold;
-    color: #333;
-    transition: color 0.3s;
-  }
-
-  .header button:hover {
-    color: #555;
-  }
-
-  table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-
-  th,
-  td {
-    padding: 12px;
-    text-align: center;
-    border: 1px solid #ccc;
-    transition: background-color 0.3s;
-  }
-
-  td {
-    cursor: pointer;
-  }
-
-  .current-month {
-    background-color: #f0f0f0;
-  }
-
-  .other-month {
-    color: #ccc;
-  }
-
-  .selected {
-    background-color: #007bff;
-    color: #fff;
-    font-weight: bold;
-  }
-</style>
-
-<div class="calendar">
-  <!-- ... Rest of the component ... -->
-</div>
-
-
-<div class="calendar">
-  <div class="header">
-    <button on:click={() => changeMonthYear(currentMonth - 1, currentYear)}>
-      Previous
-    </button>
-    <h2>{getMonthName(currentMonth)} {currentYear}</h2>
-    <button on:click={() => changeMonthYear(currentMonth + 1, currentYear)}>
-      Next
-    </button>
+<div>
+  <input type="date" bind:value={selectedDate}>
+  
+  <div>
+    <button on:click={prevMonth}>Prev</button>
+    <div>{displayedDate.toLocaleString('default', { month: 'long' })} {displayedDate.getFullYear()}</div>
+    <button on:click={nextMonth}>Next</button>
   </div>
-
+  
   <table>
     <thead>
       <tr>
@@ -119,28 +66,18 @@
       </tr>
     </thead>
     <tbody>
-      {#each $weeks as week}
-        <tr>
-          {#each week as day}
-            <td
-              class={day.date.getMonth() === currentMonth ? "current-month" : "other-month"}
-              on:click={() => updateSelectedDate(day.date)}
-            >
-              {#if day.day}
-                {day.day}
-              {/if}
-            </td>
-          {/each}
-        </tr>
-      {/each}
+      <tr>
+        {#each days as day, i (i)}
+          {#if i % 7 === 0 && i > 0}
+            </tr><tr>
+          {/if}
+          <td>
+            {#if day !== null}
+              <button on:click={() => selectDate(day)}>{day}</button>
+            {/if}
+          </td>
+        {/each}
+      </tr>
     </tbody>
   </table>
 </div>
-
-<script>
-  // Function to get the month name
-  function getMonthName(month) {
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return monthNames[month];
-  }
-</script>
--- src/routes/calendar-page.svelte
+++
@@ -1,9 +0,0 @@
-<!-- src/routes/calendar-page.svelte -->
-<script lang="ts">
-  import CalendarPicker from "./CalendarPicker.svelte";
-  import { selectedDate } from "./CalendarPicker.svelte";
-</script>
-
-<h1>Select a date:</h1>
-<input type="date" bind:value={selectedDate}>
-<CalendarPicker bind:selectedDate={selectedDate} />
